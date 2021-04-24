4962ff350167d7173e59630849245a9c57c43e25
Jan Werder <jan.werder@nordakademie.org>
2021-04-24 13:48:15 +0200

    Player movement

--- game/Player.gd
+++ game/Player.gd
@@ -17,6 +17,7 @@ const CAMERA_X_ROT_MIN = -40
 const CAMERA_X_ROT_MAX = 30
 const ROTATION_INTERPOLATE_SPEED = 10
 
+onready var initial_position = transform.origin
 onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 var camera_x_rot = 0.0
 
@@ -26,6 +27,17 @@ func _init():
 func _ready():
 	orientation = player_model.global_transform
 	orientation.origin = Vector3()
+
+func _process(_delta):
+	# Fade out to black if falling out of the map. -17 is lower than
+	# the lowest valid position on the map (which is a bit under -16).
+	# At 15 units below -17 (so -32), the screen turns fully black.
+	if transform.origin.y < -17:
+		# color_rect.modulate.a = min((-17 - transform.origin.y) / 15, 1)
+		# If we're below -40, respawn (teleport to the initial position).
+		if transform.origin.y < -40:
+		#	color_rect.modulate.a = 0
+			transform.origin = initial_position
 
 
 func _physics_process(delta):
@@ -50,34 +62,33 @@ func _physics_process(delta):
 	camera_x.y = 0
 	camera_x = camera_x.normalized()
 	
-	
-	# var diffXY = Vector2(motion.x, -motion.y)
-	
 	var target = camera_x * motion.x + camera_z * motion.y
 	if target.length() > 0.001:
 		var q_from = orientation.basis.get_rotation_quat()
 		var q_to = Transform().looking_at(target, Vector3.UP).basis.get_rotation_quat()
 		# Interpolate current rotation with desired one.
-		orientation.basis = Basis(q_from.slerp(q_to, delta * ROTATION_INTERPOLATE_SPEED))
+		orientation.basis = Basis(q_from.slerp(q_to, delta * ROTATION_INTERPOLATE_SPEED))		
 	
-	var h_velocity = orientation.origin / delta
-	velocity.x = h_velocity.x
-	velocity.z = h_velocity.z
+	animation_tree["parameters/strafe/blend_position"] = Vector2(motion.x, -motion.y)
+	animation_tree["parameters/walk/blend_position"] = Vector2(motion.length(), 0)
+	root_motion = animation_tree.get_root_motion_transform()
+	
+	var h_velocity = Transform.IDENTITY.origin / delta
+	velocity.x = -motion.x 
+	velocity.z = -motion.y
 	velocity += gravity * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
-	
-	print(motion)
-	
-	animation_tree["parameters/walk/blend_position"] = Vector2(motion.length(), 0)	
-	
-	root_motion = animation_tree.get_root_motion_transform()
-	# Apply root motion to orientation.
-	orientation *= root_motion
 	
 	orientation.origin = Vector3() # Clear accumulated root motion displacement (was applied to speed).	
 	orientation = orientation.orthonormalized() # Orthonormalize orientation.
 
 	player_model.global_transform.basis = orientation.basis
+	
+#func _input(event):
+#	if event is InputEventMouseMotion:
+#		var camera_speed_this_frame = CAMERA_MOUSE_ROTATION_SPEED
+#		rotate_camera(event.relative * camera_speed_this_frame)
+
 
 func rotate_camera(move):
 	camera_base.rotate_y(-move.x)
